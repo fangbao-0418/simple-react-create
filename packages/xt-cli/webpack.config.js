63a44897f4838f8707adc591382a8925d7bd5832
fangbao <fangbao@i-counting.cn>
2019-11-12 09:38:54 +0800

    优化

--- packages/xt-cli/webpack.config.js
+++ packages/xt-cli/webpack.config.js
@@ -1,4 +1,9 @@
 module.exports = (config, env) => {
-  console.log(config, env, 'custom config')
+  // console.log(config, env, 'custom config')
+  // if (env === 'dev') {
+
+  // } else {env === 'prod'} {
+
+  // }
   return config
 }
