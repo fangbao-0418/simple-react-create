0c1d3bbbab83774966bdec4150b87b478b83704d
fangbao <fangbao@hzxituan.com>
2020-07-30 17:23:21 +0800

    feat: 添加端口检查

--- packages/xt-cli/src/scripts/start.js
+++ packages/xt-cli/src/scripts/start.js
@@ -1,13 +1,9 @@
-/*
- * @Date: 2019-11-18 14:02:56
- * @LastEditors: fangbao
- * @LastEditTime: 2020-04-29 17:09:13
- * @FilePath: /xt-wms/Users/fangbao/Documents/xituan/xt-cli/src/scripts/start.js
- */
 // 'use strict'
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const program = require('commander')
+const detect = require('detect-port')
+
 program
   .option('-p, --port <number>', 'specified port of server')
   .option('-e, --entry <string>', 'specified entry of app')
@@ -44,7 +40,22 @@ const options = {
 }
 WebpackDevServer.addDevServerEntrypoints(webpackConfig, options)
 const compiler = webpack(webpackConfig)
-const server = new WebpackDevServer(compiler, options)
-server.listen(options.port, options.host, () => {
-  console.log('Starting server on http://' + options.host + ':' + options.port)
+
+detect(options.port, (err, _port) => {
+  if (err) {
+    console.log(err)
+  }
+  if (port === _port) {
+    startServer(port)
+  } else {
+    console.log(`port: ${port} was occupied, try port: ${_port}`)
+    startServer(_port)
+  }
 })
+
+function startServer (port) {
+  const server = new WebpackDevServer(compiler, options)
+  server.listen(port, options.host, () => {
+    console.log('Starting server on http://' + options.host + ':' + port)
+  })
+}
