95dac1fa4fcfe63d47d878ead1aa5ed969d57773
junyi <junyi.fang@tuya.com>
2021-10-07 22:25:56 +0800

    feat: edit

--- packages/xt-cli/config/argsParser.js
+++
@@ -1,8 +0,0 @@
-const yParser = require('yargs-parser')
-const args = yParser(process.argv.slice(3))
-const port = args.port || '3001'
-const entry = args.entry || ''
-module.exports = {
-  port,
-  entry
-}
--- packages/xt-cli/src/assets.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.styl' {
-  const style: {[name: string]: string}
-  export default style
-}
