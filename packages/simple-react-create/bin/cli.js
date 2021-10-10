530a9a6070f3f34262c7a1f5fdcf86b893d75b33
junyi <junyi.fang@tuya.com>
2021-10-11 00:35:27 +0800

    feat: cli

--- packages/simple-react-create/bin/cli.js
+++ packages/simple-react-create/bin/cli.js
@@ -4,9 +4,9 @@ var program = require('commander')
 var version = require('../package.json').version
 program.version(version, '-v, --version', 'output the version number');
 
-program.command('start', 'start named service', {executableFile: 'xtreact-start'})
+program.command('start', 'start named service', {executableFile: 'simple-react-create-start'})
 
-program.command('build', 'build the project', {executableFile: 'xtreact-build'})
+program.command('build', 'build the project', {executableFile: 'simple-react-create-build'})
 
 program.parse(process.argv)
 
